3ca473f7a626d8876a41c98518724573dce7c8d3
Lihle Mqhayi <2018293130@ufs4life.ac.za>
2023-04-11 22:27:56 +0200

    Add project files.

---
+++ Pythagorean triplet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class PythagoreanTriplet
+{
+    //Explanation:
+    //The program uses two nested for loops to iterate over all possible values of a and b.The value of c is then calculated as c = 1000 - a - b.
+   // If a^2 + b^2 = c^2, then the program prints the values of a, b, and c and the product of abc, and exits using the return statement.
+   //The first for loop iterates over all possible values of a, from 1 to 999, since a + b + c = 1000 and a < b < c.
+   //The second for loop iterates over all possible values of b, from a + 1 to 999, since a < b < c and b > a.
+  //When a Pythagorean triplet is found, the program prints the values of a, b, and c, as well as their product, and exits the loop using the return statement.
+    static void Main()
+    {
+        int a, b, c;
+        for (a = 1; a < 1000; a++)
+        {
+            for (b = a + 1; b < 1000; b++)
+            {
+                c = 1000 - a - b;
+                if (a * a + b * b == c * c)
+                {
+                    Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+                    Console.WriteLine("Product of abc = {0}", a * b * c);
+                    return;
+                }
+            }
+        }
+    }
+}
